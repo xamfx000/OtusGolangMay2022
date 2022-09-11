7e197b318c981344e71fa2c559a72461b3df283f
xamfx <xamfx@bk.ru>
2022-09-11 20:12:24 +0300

    ДЗ 11 Telnet Client

--- hw11_telnet_client/telnet.go
+++ hw11_telnet_client/telnet.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"io"
+	"net"
 	"time"
 )
 
@@ -12,10 +15,56 @@ type TelnetClient interface {
 	Receive() error
 }
 
-func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
-	// Place your code here.
-	return nil
+type Client struct {
+	conn    net.Conn
+	address string
+	timeout time.Duration
+	in      io.ReadCloser
+	out     io.Writer
 }
 
-// Place your code here.
-// P.S. Author's solution takes no more than 50 lines.
+func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
+	return &Client{
+		conn:    nil,
+		address: address,
+		timeout: timeout,
+		in:      in,
+		out:     out,
+	}
+}
+
+func (c *Client) Connect() (err error) {
+	conn, err := net.DialTimeout("tcp", c.address, timeout)
+	if err != nil {
+		return
+	}
+	c.conn = conn
+	return
+}
+
+func (c *Client) Send() (err error) {
+	scanner := bufio.NewScanner(c.in)
+	for scanner.Scan() {
+		_, err = c.conn.Write([]byte(scanner.Text() + "\n"))
+		if err != nil {
+			fmt.Println(err)
+			return err
+		}
+	}
+	return
+}
+
+func (c *Client) Receive() (err error) {
+	scanner := bufio.NewScanner(c.conn)
+	for scanner.Scan() {
+		_, err = c.out.Write([]byte(scanner.Text() + "\n"))
+		if err != nil {
+			return err
+		}
+	}
+	return
+}
+
+func (c *Client) Close() (err error) {
+	return c.conn.Close()
+}
